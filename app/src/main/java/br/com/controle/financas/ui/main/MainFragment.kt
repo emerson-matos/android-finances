3c834d0578dcab39f02c3ab65ab2fe8780b2959d
Emerson Almeida Matos <emegson@live.com>
2020-05-12 20:36:39 -0300

    Making recyclerview

--- app/src/main/java/br/com/controle/financas/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/controle/financas/ui/main/MainFragment.kt
@@ -1,12 +1,18 @@
 package br.com.controle.financas.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.controle.financas.R
+import br.com.controle.financas.adapter.ExpenseAdapter
+import br.com.controle.financas.data.model.ExpenseData
+import kotlinx.android.synthetic.main.expense_list.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
 
@@ -24,6 +30,10 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        expenseList.apply {
+            adapter = ExpenseAdapter(viewModel, listOf(ExpenseData(), ExpenseData(), ExpenseData()))
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
         // TODO: Use the ViewModel
     }
 
