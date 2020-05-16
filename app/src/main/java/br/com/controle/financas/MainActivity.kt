4b80e34bc581464547cdc64b8880ac0044a9c00d
Emerson Almeida Matos <emegson@live.com>
2020-05-16 15:38:09 -0300

    change name expense list package

--- app/src/main/java/br/com/controle/financas/MainActivity.kt
+++ app/src/main/java/br/com/controle/financas/MainActivity.kt
@@ -2,7 +2,7 @@ package br.com.controle.financas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.controle.financas.ui.main.MainFragment
+import br.com.controle.financas.ui.main.expense.list.ExpenseListFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
+                    .replace(R.id.container, ExpenseListFragment.newInstance())
                     .commitNow()
         }
     }
