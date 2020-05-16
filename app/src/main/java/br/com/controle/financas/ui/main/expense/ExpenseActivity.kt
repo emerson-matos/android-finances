df500c4e39dee9de5aeaa784b4587486441c021d
Emerson Almeida Matos <emegson@live.com>
2020-05-16 15:44:41 -0300

    organize expense package

--- app/src/main/java/br/com/controle/financas/ui/main/expense/ExpenseActivity.kt
+++ app/src/main/java/br/com/controle/financas/ui/main/expense/ExpenseActivity.kt
@@ -1,17 +1,18 @@
-package br.com.controle.financas
+package br.com.controle.financas.ui.main.expense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.controle.financas.R
 import br.com.controle.financas.ui.main.expense.list.ExpenseListFragment
 
-class MainActivity : AppCompatActivity() {
+class ExpenseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.expense_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, ExpenseListFragment.newInstance())
+                    .replace(R.id.expenseContainer, ExpenseListFragment.newInstance())
                     .commitNow()
         }
     }
